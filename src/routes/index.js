c5029742e9ab7a672ef70148c8a8512bd0f4ea01
thibaultcrn1 <tcarron6@outlook.com>
2022-07-09 03:12:56 +0200

    Create staff endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const clients = require('./clients');
+const staff = require('./staff');
 
 router.use('/clients', clients);
+router.use('/staff', staff);
 
 module.exports = router;
