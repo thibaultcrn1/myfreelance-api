4b0a35d5f481ccd1ba10c3a7cbb664650367c902
thibaultcrn1 <tcarron6@outlook.com>
2022-07-09 22:02:38 +0200

    Update

--- src/controllers/clients/index.js
+++
@@ -1,9 +0,0 @@
-const createClientController = require('./createClientController')
-const updateClientController = require('./updateClientController');
-const deleteClientController = require('./deleteClientController');
-
-module.exports = {
-    createClientController,
-    updateClientController,
-    deleteClientController
-};
