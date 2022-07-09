d875ed9a1d939906fccba71ad161c15d635b4c4c
thibaultcrn1 <tcarron6@outlook.com>
2022-07-09 03:01:53 +0200

    Create database schema folder

---
+++ src/database/schemas/ClientSchema.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const ClientSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: true,
+    },
+    client_number: {
+        type: mongoose.SchemaTypes.Number,
+        required: true,
+        unique: true,
+    },
+    firstname: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: false,
+    },
+    lastname: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: false,
+    },
+    email: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: true,
+    },
+    address_number: {
+        type: mongoose.SchemaTypes.Number,
+        required: true,
+        unique: false,
+    },
+    address_line: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: false,
+    },
+    zip_code: {
+        type: mongoose.SchemaTypes.Number,
+        required: true,
+        unique: true,
+    }
+})
+
+module.exports + mongoose.model('ClientRegistered', ClientSchema);
