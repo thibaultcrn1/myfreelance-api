dd48858d614318174944c9100e3aa27d49a23c1f
thibaultcrn1 <tcarron6@outlook.com>
2022-07-09 03:06:59 +0200

    Create StaffSchema & Update Client Schema

---
+++ src/database/schemas/StaffSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const StaffSchema = new mongoose.Schema({
+    id: {
+        type: mongoose.SchemaTypes.Number,
+        required: true,
+        unique: true,
+    },
+    firstname: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: false,
+    },
+    lastname: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: false,
+    },
+    email: {
+        type: mongoose.SchemaTypes.String,
+        required: true,
+        unique: true,
+    },
+    isAdmin: {
+        type: mongoose.SchemaTypes.Boolean,
+        required: true,
+        default: false,
+        unique: false,
+    }
+});
+
+module.exports = mongoose.model('StaffRegistered', StaffSchema);
