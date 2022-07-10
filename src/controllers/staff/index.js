f7ee8fd20b923a647090d10c6d12a2a6c685ab8e
thibaultcrn1 <tcarron6@outlook.com>
2022-07-10 16:07:36 +0200

    Create update function for update staff route

--- src/controllers/staff/index.js
+++ src/controllers/staff/index.js
@@ -31,6 +31,32 @@ async function createStaffController(req, res) {
 
 async function updateStaffController(req, res) {
 
+    const { id, firstname, lastname, email, password, isAdmin } = req.body;
+
+    if(!id) return res.status(400).send({ msg: 'ID Required' });
+    if(!firstname) return res.status(400).send({ msg: 'FIRSTNAME Required' });
+    if(!lastname) return res.status(400).send({ msg: 'LASTNAME Required' });
+    if(!email) return res.status(400).send({ msg: 'EMAIL Required' });
+    if(!password) return res.status(400).send({ msg: 'PASSWORD Required' });
+    if(!isAdmin) return res.status(400).send({ msg: 'ISADMIN Required' });
+
+    const encryptedPassword = await encrypt(password);
+
+    await StaffSchema.findOneAndDelete({ id: id });
+
+    const update = await new StaffSchema({
+        id: id,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: encryptedPassword,
+        isAdmin: isAdmin,
+    });
+
+    return update.save()
+    .then((updatedStaff) => res.status(200).send({ updatedStaff }))
+    .catch((err) => res.status(400).send({ err }));
+
 }
 
 async function deleteStaffController(req, res) {
