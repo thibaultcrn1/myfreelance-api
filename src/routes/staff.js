cdc7407559ea618572a9c5d90b6b12e927f5a357
thibaultcrn1 <tcarron6@outlook.com>
2022-07-10 18:22:26 +0200

    Update staff route

--- src/routes/staff.js
+++ src/routes/staff.js
@@ -8,20 +8,8 @@ router.get('/', (req, res) => {
     res.status(200).send({ msg: 'hello world!' });
 })
 
-router.put('/create', async(req, res) => createStaffController(req, res));
-router.put('/update', async(req, res) => updateStaffController(req, res));
-router.put('/delete', async(req, res) => deleteStaffController(req, res));
-
-router.put('/password', async(req, res) => {
-
-    const { password } = req.body;
-
-    if(!password) return res.status(400).send({ msg: 'PASSWORD Required' })
-
-    const encryptedPassword = encrypt(password);
-
-    return res.status(200).send({ encryptedPassword });
-
-})
+router.put('/create', async(req, res) => await createStaffController(req, res));
+router.put('/update', async(req, res) => await updateStaffController(req, res));
+router.put('/delete', async(req, res) => await deleteStaffController(req, res));
 
 module.exports = router;
