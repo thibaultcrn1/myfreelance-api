1d572ae6bb7b93f20474a2ef14c0a7c44718ffef
thibaultcrn1 <tcarron6@outlook.com>
2022-07-18 04:10:19 +0200

    Update

--- src/database/schemas/index.js
+++ src/database/schemas/index.js
@@ -1,6 +1,11 @@
 const StockSchema = require('./stock.schemas');
 const CategorySchema = require('./category.schemas');
+const SessionsSchema = require('./sessions.schemas');
+const UsersSchema = require('./users.schemas');
 
 module.exports = {
-    StockSchema
+    StockSchema,
+    CategorySchema,
+    SessionsSchema,
+    UsersSchema
 }
