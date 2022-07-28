fa7d14b4492ecc5be4ac8de1e8f16f6c33c853fd
thibaultcrn1 <tcarron6@outlook.com>
2022-07-29 01:56:45 +0200

    Delete utils

--- src/utils/decrypt.js
+++
@@ -1,7 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-function decrypt(password) {
-    return CryptoJS.AES.decrypt(password, process.env.CRYPTOJS_HASH_PASSPHRASE);
-};
-
-module.exports = { decrypt };
--- src/utils/encrypt.js
+++
@@ -1,7 +0,0 @@
-const CryptoJS = require('crypto-js');
-
-function encrypt(password) {
-    return CryptoJS.AES.encrypt(password, process.env.CRYPTOJS_HASH_PASSPHRASE);
-};
-
-module.exports = { encrypt };
