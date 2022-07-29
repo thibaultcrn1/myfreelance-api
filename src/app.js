fb8e8068f930aa2c88afab1a3b20d4aa7549b4b8
thibaultcrn1 <tcarron6@outlook.com>
2022-07-29 13:47:04 +0200

    Edit motd

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
 
+const API_URI = "http://localhost:3001/api";
+
+console.log(" _  _  _  _  ____  ____  ____  ____  __     __   __ _   ___  ____ ");
+console.log("( \\/ )( \\/ )(  __)(  _ \\(  __)(  __)(  )   / _\\ (  ( \\ / __)(  __)");
+console.log("/ \\/ \\ )  /  ) _)  )   / ) _)  ) _) / (_/\\/    \\/    /( (__  ) _) ");
+console.log("\\_)(_/(__/  (__)  (__\\_)(____)(____)\\____/\\_/\\_/\\_)__) \\___)(____)");
+console.log(' ');
+console.log('[INFO] ' + API_URI + "/auth/register");
+console.log('[INFO] ' + API_URI + "/auth/login");
+console.log('[INFO] ' + API_URI + "/auth/logout");
+console.log('[INFO] ' + API_URI + "/auth/refreshtoken");
+console.log('[INFO] ' + API_URI + "/auth/users");
+console.log(' ');
+console.log('[INFO] ' + API_URI + "/category/create");
+console.log('[INFO] ' + API_URI + "/category/update");
+console.log('[INFO] ' + API_URI + "/category/delete");
+console.log(' ');
+console.log('[INFO] ' + API_URI + "/invoice/create");
+console.log('[INFO] ' + API_URI + "/invoice/update");
+console.log('[INFO] ' + API_URI + "/invoice/delete");
+console.log(' ');
+console.log('[INFO] ' + API_URI + "/stock/create");
+console.log('[INFO] ' + API_URI + "/stock/update");
+console.log('[INFO] ' + API_URI + "/stock/delete");
+console.log(' ');
+
 mongoose.connect(process.env.MONGODB, {
     useUnifiedTopology: true,
     useNewUrlParser: true
