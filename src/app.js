eb68ba671a7ab8301232b15e11aab870960376e1
thibaultcrn1 <tcarron6@outlook.com>
2022-07-31 00:44:06 +0200

    Install cors and install on project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -51,6 +52,11 @@ mongoose.connection.on('err', (err) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: ['http://localhost:4200'],
+    credentials: true,
+}));
 
 app.use('/api', routes);
 
