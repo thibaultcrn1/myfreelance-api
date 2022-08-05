ce5d3f3a83518df2fb67d57b43c8600bc08ce582
thibaultcrn1 <tcarron6@outlook.com>
2022-08-05 19:44:48 +0200

    Add all controllers on indexer

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -2,10 +2,14 @@ const { registerControllers } = require('./register.controllers');
 const { loginControllers } = require('./login.controllers');
 const { refreshTokenControllers } = require('./refreshtoken.controllers');
 const { logoutControllers } = require('./logout.contollers');
+const { forgotPasswordControllers } = require('./forgot-password.controllers');
+const { resetPasswordControllers } = require('./reset-password.controllers')
 
 module.exports = {
     registerControllers,
     loginControllers,
     refreshTokenControllers,
-    logoutControllers
+    logoutControllers,
+    forgotPasswordControllers,
+    resetPasswordControllers
 };
