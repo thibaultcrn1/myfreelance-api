b12017163dc9d36499aa9cc8a781d0eb259edd69
thibaultcrn1 <tcarron6@outlook.com>
2022-09-03 13:03:26 +0200

    Update inderer of schema

--- src/database/schemas/index.js
+++ src/database/schemas/index.js
@@ -1,11 +1,9 @@
 const StockSchema = require('./stock.schemas');
 const CategorySchema = require('./category.schemas');
-const SessionsSchema = require('./sessions.schemas');
 const UsersSchema = require('./users.schemas');
 
 module.exports = {
     StockSchema,
     CategorySchema,
-    SessionsSchema,
     UsersSchema
 }
