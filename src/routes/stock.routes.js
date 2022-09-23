762b50b447e43d2e9f4893d1e8ce6b110a4a7634
thibaultcrn1 <tcarron6@outlook.com>
2022-09-23 08:36:37 +0200

    Update all

--- src/routes/stock.routes.js
+++ src/routes/stock.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const { createStock, updateStock, deleteStock } = require('../controllers/stock');
+const { createStock, updateStock, deleteStock, getStock } = require('../controllers/stock');
 
 router.post('/create', (req, res) => createStock(req, res));
 router.put('/update', (req, res) => updateStock(req, res));
 router.delete('/delete', (req, res) => deleteStock(req, res));
+router.get('/get', (req, res) => getStock(req, res));
 
 module.exports = router;
