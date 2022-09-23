011efb5a8c0e00ccaff4a3cdcc9414cdbb7ff121
thibaultcrn1 <tcarron6@outlook.com>
2022-09-23 08:36:12 +0200

    Update

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -3,9 +3,9 @@ const passport = require('passport');
 
 const { createCategory, modifyCategory, deleteCategory, getCategory } = require('../controllers/category');
 
-router.post('/create', passport.authenticate('jwt', { session: true }), (req, res) => createCategory(req, res));
-router.put('/update', passport.authenticate('jwt', { session: true }), (req, res) => modifyCategory(req, res));
-router.delete('/delete', passport.authenticate('jwt', { session: true }), (req, res) => deleteCategory(req, res));
-router.get('/get', passport.authenticate('jwt', { session: true }), (req, res) => getCategory(req, res));
+router.post('/create', (req, res) => createCategory(req, res));
+router.put('/update', (req, res) => modifyCategory(req, res));
+router.delete('/delete', (req, res) => deleteCategory(req, res));
+router.get('/get', (req, res) => getCategory(req, res));
 
 module.exports = router;
