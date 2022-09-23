1e24f176675e088030f85ec06c4d1f722ebfbde1
thibaultcrn1 <tcarron6@outlook.com>
2022-09-23 08:35:51 +0200

    Update index.js

--- src/controllers/stock/index.js
+++ src/controllers/stock/index.js
@@ -1,10 +1,12 @@
 const { createStock } = require('./createstock.controllers');
 const { deleteStock } = require('./deletestock.controllers');
 const { updateStock } = require('./updatestock.controllers');
+const { getStock } = require('./getstock.controllers');
 
 module.exports = {
     createStock,
     deleteStock,
-    updateStock
+    updateStock,
+    getStock
 };
 
