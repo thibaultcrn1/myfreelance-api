c2458793b6f39975896dd07159a451a602f5a75a
thibaultcrn1 <tcarron6@outlook.com>
2022-11-01 17:41:41 +0100

    Update invoice route

--- src/routes/invoice.routes.js
+++ src/routes/invoice.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
-const { createInvoice, updateInvoice, deleteInvoice } = require('../controllers/invoice');
+const { createInvoice, updateInvoice, deleteInvoice, getInvoice } = require('../controllers/invoice');
 
 router.post('/create', (req, res) => createInvoice(req, res));
 router.put('/update', (req, res) => updateInvoice(req, res));
 router.delete('/delete', (req, res) => deleteInvoice(req, res));
+router.get('/get', (req, res) => getInvoice(req, res));
 
 module.exports = router;
