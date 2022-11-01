acd46c4a9ac30138fbef0e2edcf34fff3ad2f8dd
thibaultcrn1 <tcarron6@outlook.com>
2022-11-01 17:42:04 +0100

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,13 @@ const auth = require('./auth.routes');
 const invoice = require('./invoice.routes');
 const category = require('./category.routes');
 const tva = require('./tva.routes');
+const seller = require('./seller.routes');
 
 router.use('/stock', stock);
 router.use('/category', category);
 router.use('/auth', auth);
 router.use('/invoice', invoice);
 router.use('/tva', tva);
+router.use('/seller', seller);
 
 module.exports = router;
